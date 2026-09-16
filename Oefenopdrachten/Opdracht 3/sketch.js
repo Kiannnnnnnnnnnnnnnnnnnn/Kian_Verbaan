b8cc8b352694b9290578ea31294e195f2f695cc0
Kiannnnnnnnnnnnnnnnnnnn <verbaankian@outlook.com>
2026-09-16 15:31:47 +0200

    ha

--- Oefenopdrachten/Opdracht 3/sketch.js
+++ Oefenopdrachten/Opdracht 3/sketch.js
@@ -1,7 +1,23 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 200);
 }
+
+
 
 function draw() {
   background(220);
+let score = random(0, 100);
+noLoop();
+  fill(100, 100, 100);
+  textSize(20);
+
+  if (score >= 90) {
+  text("Uitstekend", 200, 100);
+  } else if (score > 70 && score < 89) {
+  text("goed gedaan", 200, 100);
+  } else if (score > 50 && score < 69) {
+  text("voldoende", 200, 100);
+  } else {
+  text("onvoldoende", 200, 100);
+  }
 }
--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,7 +1,28 @@
+let x = 100
+let y = 80
+let greeting = "Hello World!"
+let a = 20
+let b = 10
+let optellen = a + b
+let aftrekken = a - b
+let vermenigvuldigen = a * b
+let delen = a / b
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+
+  text(x, 20, 20);
+  text(greeting, 20, 60);
+
+  text("Optellen = " + optellen, 20, y);
+  noLoop(y = y+ 20);
+  text("Aftrekken = " + aftrekken, 20, y);
+  noLoop(y = y + 20);
+  text("Vermenigvuldigen = " + vermenigvuldigen, 20, y);
+  noLoop(y = y + 20);
+  text("Delen = " + delen, 20, y);
 }
